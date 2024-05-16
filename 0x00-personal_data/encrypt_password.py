563813c70fa0064cb5c1e8e91b8f70c337be651b
mugishab2020 <mugishab2020@gmail.com>
2024-05-16 14:29:40 +0200

    May be it can work

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,7 +4,7 @@
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str):
     '''Encrypting the password using bcrypt'''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
---
+++ README.md
@@ -0,0 +1,2 @@
+This repo is for understanding more about user data , authentication and configuration
+authorization in Backend using python
