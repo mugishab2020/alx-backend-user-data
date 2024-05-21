382c200402409eb5a166713be5c83f47a1c85997
mugishab2020 <mugishab2020@gmail.com>
2024-05-21 16:50:03 +0200

    May be the ahey are true

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,7 +14,7 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes=False)
+@app_views.route('/stats', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
     Return:
@@ -24,3 +24,16 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', method=['GET'] ,strict_slashes=False)
+def unauthorized_request() -> str:
+    '''unthorised request'''
+    abort(401)
+
+
+@app_views.route('/forbidden', method=['GET'], strict_slashes=False)
+def forbidden_request() -> str:
+    '''forbidden request'''
+    abort(403)
+
