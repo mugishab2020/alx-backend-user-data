e1663f3337234362fa8976d6f6b4a33e6d97383f
mugishab2020 <mugishab2020@gmail.com>
2024-05-28 21:30:14 +0200

    We are starting

---
+++ 0x03-user_authentication_service/README.md
@@ -0,0 +1,1 @@
+This is the project that we going to see user authentication in details
---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+
+from user import Base, User
+
+
+class DB:
+    """DB class
+    """
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
+    
+    def find_user_by(self, **kwargs) -> User:
+        session = self._session()
+        try:
+            user = session.query(User).filter_by(**kwargs).first()
+        except NoResultFound:
+            session.close()
+            raise NoResultFound()
+        except InvalidRequestError:
+            session.close()
+            raise InvalidRequestError()
+        session.close()
+        return user
+    
