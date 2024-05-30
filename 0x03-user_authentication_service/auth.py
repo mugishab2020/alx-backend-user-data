4878927e5f93a0af2a1ebb830decd41dd6f9519c
mugishab2020 <mugishab2020@gmail.com>
2024-05-30 18:27:14 +0200

    Now we have the update password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,3 +78,12 @@ class Auth:
             return uid
         except Exception:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed_password = _hash_password(password=password)
+            self._db.update_user(user, _hash_password=hashed_password)
+            self._db.update_user(user, reset_token=None)
+        except Exception:
+            raise ValueError
